16a9be0dbc4315f1f3b5cd14ebf1d6f4f995b6c2
Tanner Crook <tannercrook@gmail.com>
2022-04-27 22:50:06 -0600

    Removed uneeded file.

--- intelliherd/models/DBase.py
+++
@@ -1,8 +0,0 @@
-# PostgreSQL
-import psycopg2
-from psycopg2 import extras
-
-def connection():
-    conn = psycopg2.connect(host="meridia.crooktec.com",database="intelliherd",user="tannercrook",password="Kinger413!")
-    c = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    return c, conn
